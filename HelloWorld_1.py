fdc7c5f30c3f9de5524759e5601417a7aa78a99f
Tarif Malik <88896675+tmalik78@users.noreply.github.com>
2021-09-10 22:15:44 +0600

    This is initial file - HW 1 Send line added Variable added new line & special chars added New line added with new variable

--- HelloWorld_1.py
+++ HelloWorld_1.py
@@ -1,6 +1,6 @@
 # comment added here
-print ("Hello World 1")
-myStr = "\'hello \nVikash\'"
+print("Hello World 1")
+myStr = "\'hello \nVikash!\'"
+myStr1 = "\'hello \nTarif!\'"
 print(myStr)
-
-
+print(myStr1)
