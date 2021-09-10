fff9b522c8910bb9e1e77483d3eaaa9c66c3efcb
tmalik78 <tarif.malik@gmail.com>
2021-09-10 23:10:31 +0600

    This is initial file - HW 1 Send line added Variable added new line & special chars added New line added with new variable

--- Codes/HelloWorld_1.py
+++ Codes/HelloWorld_1.py
@@ -1,4 +1,8 @@
-# comment added here
+# A basic Hello world program
+'''
+Python version 3.8
+Remote Git -> Practice1 repository
+'''
 print("Hello World 1")
 myStr = "\'hello \nVikash!\'"
 myStr1 = "\'hello \nTarif!\'"
